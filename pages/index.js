c82813a018b82d5934e28faf6daec28a3f2f665e
Sergio Martin Alexander Villagomez Ortega <martin.v.ortega@gmail.com>
2020-09-29 00:18:33 -0500

    Add project

---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+import AppLayout from '../components/AppLayout'
+
+
+export default function Home() {
+  const router = useRouter()
+
+  return (
+    <>
+      <Head>
+        <title>devter 🐦</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <AppLayout>
+        <h1>
+          <a href="https://nextjs.org">devter</a>
+        </h1>
+      </AppLayout>
+
+      <style jsx>{`
+        h1 {
+          text-align: center;
+          font-size: 48px;
+        }
+
+        nav {
+          font-size: 24px;
+          text-align: center;
+        }
+
+        .another-title {
+          color: #333;
+          font-size: 24px;
+        }
+
+        a {
+          color: orange;
+          text-decoration: none;
+        }
+      `}</style>
+    </>
+  )
+}
